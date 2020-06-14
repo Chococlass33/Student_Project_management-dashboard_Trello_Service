bcb778376c5b798abc834c16cc21e5e02fb6701e
Quinn <quinn@jelly-snakes.com>
2020-06-15 03:52:42 +1000

    Generate list of boards

--- frontendcomponents/src/Components/Screens/AddBoard.js
+++ frontendcomponents/src/Components/Screens/AddBoard.js
@@ -13,25 +13,66 @@ class AddBoard extends Component {
         super(props);
         this.state = {
             token: this.props.location.hash ? queryString.parse(this.props.location.hash).token : undefined,
-            projectId: undefined
+            projectId: undefined,
+            boards: undefined
         };
         if (this.props.location.state) {
-            this.state.projectId  = this.props.location.state.projectId
+            this.state.projectId = this.props.location.state.projectId
         } else {
             const values = queryString.parse(this.props.location.search)
             this.state.projectId = values.projectId
         }
     }
 
+    componentDidMount() {
+        if (this.state.token) {
+            console.log("Fetching Boards")
+            fetch(`https://api.trello.com/1/members/me/boards?fields=name,url&key=${API_KEY}&token=${this.state.token}`)
+                .then(res => res.json())
+                .then((result) => {
+                        console.log("done")
+                        this.setState((state, props) => {
+                            return  {
+                                ...state,
+                                boards: result
+                            };
+                        })
+                    },
+                    (error) => {
+                        console.log("error:" + error)
+                        this.setState((state, props) => {
+                            return state + {boards: undefined};
+                        })
+                    })
+        }
+    }
+
     render() {
         /* Check if we have a token */
         if (this.state.token && this.state.projectId) {
-            return (
-                <div>
-                    <h1 style={{marginTop: "64px"}}>Add Board</h1>
-                    <p>Authorised using token: {this.state.token} for projectId {this.state.projectId}</p>
-                </div>
-            );
+            console.log("boards: " + this.state.boards)
+            if (this.state.boards) {
+                const boards = this.state.boards.map((board, i) =>
+                    (<div>
+                        Board: {board.name}<br/>
+                        <a target="_blank"  href={board.url}>Board URL</a>
+                        <button onClick={this.useBoard.bind(this, board)}>Use Board</button>
+                    </div>)
+                );
+                return (
+                    <div>
+                        <h1 style={{marginTop: "64px"}}>Add Board</h1>
+                        <tbody>{boards}</tbody>
+                    </div>
+                )
+            } else {
+                return (
+                    <div>
+                        <h1 style={{marginTop: "64px"}}>Add Board</h1>
+                        <p>Loading trello boards</p>
+                    </div>
+                );
+            }
         } else if (this.state.projectId) {
             /* Go get one */
             return (
@@ -63,6 +104,10 @@ class AddBoard extends Component {
             `&return_url=${URL}/addBoard?projectId=${projId}` +
             "&response_type=token" +
             `&key=${API_KEY}`
+    }
+
+    useBoard(board) {
+        console.log(board)
     }
 }
 
