2575385bc006742b60f79603dc190ad2a12c1e7c
Quinn <quinn@jelly-snakes.com>
2020-06-14 22:45:18 +1000

    Comment out broken placeholder code

--- src/main/java/com/spmd/trello/RestControl.java
+++ src/main/java/com/spmd/trello/RestControl.java
@@ -16,43 +16,43 @@ import org.springframework.web.bind.annotation.RestController;
 class RestControl {
 
     // Aggregate root
-
-    @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
-    }
-
-    @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(newEmployee);
-    }
-
-    // Single item
-
-    @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-
-        return repository.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException(id));
-    }
-
-    @PutMapping("/employees/{id}")
-    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-
-        return repository.findById(id)
-                .map(employee -> {
-                    employee.setName(newEmployee.getName());
-                    employee.setRole(newEmployee.getRole());
-                    return repository.save(employee);
-                })
-                .orElseGet(() -> {
-                    newEmployee.setId(id);
-                    return repository.save(newEmployee);
-                });
-    }
-
-    @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
-        repository.deleteById(id);
-    }
+//
+//    @GetMapping("/employees")
+//    List<Employee> all() {
+//        return repository.findAll();
+//    }
+//
+//    @PostMapping("/employees")
+//    Employee newEmployee(@RequestBody Employee newEmployee) {
+//        return repository.save(newEmployee);
+//    }
+//
+//    // Single item
+//
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
+//
+//    @PutMapping("/employees/{id}")
+//    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .map(employee -> {
+//                    employee.setName(newEmployee.getName());
+//                    employee.setRole(newEmployee.getRole());
+//                    return repository.save(employee);
+//                })
+//                .orElseGet(() -> {
+//                    newEmployee.setId(id);
+//                    return repository.save(newEmployee);
+//                });
+//    }
+//
+//    @DeleteMapping("/employees/{id}")
+//    void deleteEmployee(@PathVariable Long id) {
+//        repository.deleteById(id);
+//    }
 }
