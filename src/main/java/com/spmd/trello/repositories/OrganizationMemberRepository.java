83f8bee3ad0a116f08c0bffbe0ec15eb0dd309e4
Quinn <quinn@jelly-snakes.com>
2020-06-16 03:41:25 +1000

    Strip out org stuff

--- src/main/java/com/spmd/trello/repositories/OrganizationMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spmd.trello.repositories;
-
-import com.spmd.trello.model.Organization;
-import com.spmd.trello.model.OrganizationMember;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrganizationMemberRepository extends CrudRepository<OrganizationMember, String> {
-}
