82deb91871088bbe38531cf65b6f9eea9fca5f8d
Quinn <quinn@jelly-snakes.com>
2020-06-16 00:34:08 +1000

    Change name of the model id to be consistent

--- src/main/java/com/spmd/trello/trelloModel/NewWebhook.java
+++ src/main/java/com/spmd/trello/trelloModel/NewWebhook.java
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NewWebhook {
-    public String modelId;
+    public String idModel;
     public String token;
 }
