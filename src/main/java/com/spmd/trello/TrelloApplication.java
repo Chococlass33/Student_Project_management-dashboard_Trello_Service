61a6b8fea884602ac003d453c079147c03e9c6b8
Quinn <quinn@jelly-snakes.com>
2020-06-16 22:51:30 +1000

    more url hardcode

--- src/main/java/com/spmd/trello/TrelloApplication.java
+++ src/main/java/com/spmd/trello/TrelloApplication.java
@@ -2,44 +2,19 @@ package com.spmd.trello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class TrelloApplication implements ApplicationRunner {
+public class TrelloApplication {
     private static final Logger logger = LoggerFactory.getLogger(TrelloApplication.class);
 
-    private String frontendUrl;
-    public static String backendUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(TrelloApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.defaultHeader("Accept", "application/json").build();
-    }
-
-    @Override
-    public void run(ApplicationArguments args) {
-        logger.info("Application started with command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
-        logger.info("NonOptionArgs: {}", args.getNonOptionArgs());
-        logger.info("OptionNames: {}", args.getOptionNames());
-
-        frontendUrl = args.getOptionValues("frontend").get(0);
-        backendUrl = args.getOptionValues("backend").get(0);
     }
 
     @Bean
@@ -47,7 +22,7 @@ public class TrelloApplication implements ApplicationRunner {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3002", frontendUrl, "http://167.99.7.70:3002");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3002", "http://167.99.7.70:3002");
             }
         };
     }
