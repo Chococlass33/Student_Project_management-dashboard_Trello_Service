1a6f720b5ace132f7e4f15175bb263eee6fd0940
Kenny Huynh <kennyhuynh1198@gmail.com>
2020-09-05 19:28:11 +1000

    Update BoardController.java
    - Considered case where card is updated and moved from one list to another. The returned Board object will contain a set of cards & lists that are within the set dates (to do).

--- src/main/java/com/spmd/trello/controller/BoardController.java
+++ src/main/java/com/spmd/trello/controller/BoardController.java
@@ -1,13 +1,19 @@
 package com.spmd.trello.controller;
 
 import com.spmd.trello.BadConfig;
+import com.spmd.trello.model.Action;
 import com.spmd.trello.model.Board;
+import com.spmd.trello.model.Card;
+import com.spmd.trello.model.List;
 import com.spmd.trello.repositories.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 
 //TBD, currently stock placeholder from springboot tutorial until i get the entities right
@@ -38,12 +44,13 @@ class BoardController {
     }
 
     @GetMapping("/board/{id}")
-    boolean boardHistory(@PathVariable String id, @RequestParam("date") Optional<Date> date) {
+    Board boardHistory(@PathVariable String id, @RequestParam("date") Optional<Date> date) {
         Optional<Board> retrievedBoard = repository.findById(id);
 
-        boolean output = false;
-        // If board and date are found: perform handleBoardHistory(), otherwise return BoardNotFoundException.
-        output = retrievedBoard.isPresent() && date.isPresent() ? handleBoardHistory(retrievedBoard.get(), date.get()) : false;
+        Board output;
+        // If board and date are found: perform handleBoardHistory(), otherwise return BoardNotFoundException (or original board).
+        output = retrievedBoard.isPresent() && date.isPresent()
+                ? handleBoardHistory(retrievedBoard.get(), date.get()) : repository.findById(id).orElseThrow();
         return output;
     }
 
@@ -52,12 +59,57 @@ class BoardController {
         repository.deleteById(id);
     }
 
-    private boolean handleBoardHistory(Board board, Date date) {
+    private Board handleBoardHistory(Board board, Date date) {
         // Now there is a valid board and date (which should be ISO8601 compliant from front-end).
         // Now we can use the date to handle and filter actions to a specific time.
         // Can't continue until method of reconstructing board is found (either through database entries of list/card etc..
         // Or through webhook actions and reconstructing through a series of actions.
+        Set<Action> actions = board.getActions();
+        Set<Action> filteredActions = new HashSet<>();
 
-        return true;
+        // Filter actions.
+        for (Action action: actions
+             ) {
+            // If the date of the action is before the requested date, then add the action to the filtered actions set.
+            if (new Date(action.getDateCreated().getTime()).before(date)) {
+                filteredActions.add(action);
+
+            }
+        }
+
+        // To Do:
+        // Filter cards.
+        // Filter lists.
+        Set<List> lists = board.getLists();
+        Set<Card> cards = board.getCards();
+
+        // Parse json string from action.data into object and use the data thereafter.
+
+        // Now we have:
+        // - the set of actions that can recreate the state of the board
+        // - relevant lists of the board
+        // - relevant cards of the board
+        // Consider case where card is updated.
+        for (Action filteredAction : filteredActions) {
+            if (isCardMoved(filteredAction)) {
+                // Set the new list of the card.
+                for (Card card : cards) {
+                    if (card.getId() == filteredAction.getData().card.id) {
+                        card.setList(filteredAction.getData().listAfter);
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Finally, after all modifications, set new list and cards to board, for eventual display.
+        board.setLists(lists);
+        board.setCards(cards);
+
+        return board;
+    }
+
+    private boolean isCardMoved(Action action) {
+        return action.getType() == "updateCard" ? action.getData().listBefore && action.getData().listAfter : false;
     }
 }
