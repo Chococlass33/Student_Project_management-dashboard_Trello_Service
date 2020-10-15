96a139a1914cd6fdd7cf92f92bdbcfc7373ecddc
Kenny Huynh <kennyhuynh1198@gmail.com>
2020-10-15 20:06:57 +1100

    Creating models to capture data

--- src/main/java/com/spmd/trello/controller/DataExportController.java
+++ src/main/java/com/spmd/trello/controller/DataExportController.java
@@ -15,5 +15,35 @@ public class DataExportController {
         this._actionRepository = actionRepository;
     }
 
+    /**
+     * Represents the structure of the Trello data/action to export.
+     */
+    private static class TrelloDataExport {
 
+        // User information.
+        public String memberId;
+        public String fullName;
+        public String email;
+
+        // Action information.
+        public String actionType;
+        public String actionData; // This data should be different based on the action.
+    }
+
+    /**
+     * Represents a subset of the structure of the response from getting all members of a board from the Trello API.
+     * This will only contain an ID as a means of subsequently and individually accessing a member's details.
+     */
+    private static class BoardMemberResponse {
+        public String memberId;
+    }
+
+    /**
+     * Represents a subset of the structure of the response from getting a particular member from the Trello API.
+     */
+    private static class MemberResponse {
+        public String memberId;
+        public String fullName;
+        public String email;
+    }
 }
