645e5c41084aa1c649591d9f5794c133f2b61b5e
Chee Chin Chan <ccha0048@student.monash.edu>
2020-11-10 01:04:59 +1100

    Fixed activity controller to request member info from trello

--- src/main/java/com/spmd/trello/controller/ActivityController.java
+++ src/main/java/com/spmd/trello/controller/ActivityController.java
@@ -1,15 +1,22 @@
 package com.spmd.trello.controller;
 
+import com.spmd.trello.BadConfig;
+import com.spmd.trello.RawBoard;
 import com.spmd.trello.database.Action;
 import com.spmd.trello.database.ActionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,11 +45,38 @@ public class ActivityController {
     }
 
     @GetMapping("/activity/WIP")
-    ResponseEntity<List<Lastmodified>> boardHistoryWIP(@RequestParam String emails, @RequestParam String boardIds) {
+    ResponseEntity<List<Lastmodified>> boardHistoryWIP(@RequestParam String emails, @RequestParam String boardIds, @RequestParam String token) {
 
         //Create the list of emails and board ids.
         String[] emailList = emails.split(",", 0);
         String[] boardList = boardIds.split(",", 0);
+
+        List<BoardMemberResponse> listOfMembersOfABoard = new ArrayList<>();
+        for (int i = 0; i < boardList.length; i++)
+        {
+            URI url = UriComponentsBuilder.fromHttpUrl("https://api.trello.com/1/boards/" + boardList[i] + "/members")
+                    .queryParam("key", BadConfig.API_KEY)
+                    .queryParam("token", token)
+                    .build().toUri();
+            RestTemplate restTemplate = new RestTemplate();
+
+            ResponseEntity<List<BoardMemberResponse>> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>(){});
+            listOfMembersOfABoard.addAll(responseEntity.getBody());
+        }
+
+        HashMap<String, MemberResponse> mapOfMembers = new HashMap<>();
+        listOfMembersOfABoard.stream()
+                .filter(member -> member != null) // Check if the particular member is null, and if so, filter it out.
+                .forEach(member -> {
+                    ResponseEntity<MemberResponse> response = GetMember(member.memberId, token);
+
+                    if (response != null) {
+                        mapOfMembers.put(response.getBody().memberId, response.getBody());
+                    }
+                });
+
+
+
 
 
         //Create a mapping for each email given
@@ -56,22 +90,22 @@ public class ActivityController {
         for (String boardId : boardList) {
             actions.addAll(actionRepository.findAllByBoard(boardId));
         }
-//
-//        //For each action, try to see if it's member's email is present in the above map, if so then get the date and compare the latest one
-//        for (Action action : actions) {
-//            //Check for member in database
-//            Optional<Member> member = memberRepository.findById(action.getMember());
-//            if (member.isPresent()) {
-//                //Check for this member's email in emailmap
-//                if (emailMap.get(member.get().getEmail()) != null) {
-//                    //Compare current time
-//                    if (action.getDateCreated().getTime() > emailMap.get(member.get().getEmail()).getTime()) {
-//                        //If the action is sooner, then replace the map.
-//                        emailMap.put(member.get().getEmail(), action.getDateCreated());
-//                    }
-//                }
-//            }
-//        }
+
+        //For each action, try to see if it's member's email is present in the above map, if so then get the date and compare the latest one
+        for (Action action : actions) {
+            //Check for member in database
+            MemberResponse memberResponse = mapOfMembers.get(action.getMember());
+            if (memberResponse != null) {
+                //Check for this member's email in emailmap
+                if (emailMap.get(memberResponse.email) != null) {
+                    //Compare current time
+                    if (action.getDateCreated().getTime() > emailMap.get(memberResponse.email).getTime()) {
+                        //If the action is sooner, then replace the map.
+                        emailMap.put(memberResponse.email, action.getDateCreated());
+                    }
+                }
+            }
+        }
 
         //List to store response in
         List<Lastmodified> response = new ArrayList<>();
@@ -102,6 +136,40 @@ public class ActivityController {
             this.date = date;
         }
 
+    }
+
+    /**
+     * Represents a subset of the structure of the response from getting all members of a board from the Trello API.
+     * This will only contain an ID as a means of subsequently and individually accessing a member's details.
+     */
+    private static class BoardMemberResponse {
+        public String memberId;
+    }
+
+    /**
+     * Represents a subset of the structure of the response from getting a particular member from the Trello API.
+     */
+    private static class MemberResponse {
+        public String memberId;
+        public String fullName;
+        public String email;
+    }
+
+    private ResponseEntity<MemberResponse> GetMember(String memberId, String token) {
+        URI url = UriComponentsBuilder.fromHttpUrl("https://api.trello.com/1/members/" + memberId)
+                .queryParam("key", BadConfig.API_KEY)
+                .queryParam("token", token)
+                .build().toUri();
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<MemberResponse> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>(){});
+
+        // If there is no information found on the particular member, return a response entity with Http status NOT FOUND.
+        if (responseEntity == null) {
+            return null;
+        }
+
+        return responseEntity;
     }
 
 
